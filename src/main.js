ddf5e32d13d396186b26df701bee24df9b8e3b81
Egor1sh <egorovshahmati@mail.ru>
2026-06-29 23:29:51 +0000

    dima

--- src/main.js
+++ src/main.js
@@ -17,7 +17,10 @@ const students = [
 
   { id: "11", name: "Дрябов Андрей", folder: "11_Дрябов_Андрей", avatar: "24.jpg", photos: [] },
   { id: "12", name: "Дымченко Иван", folder: "12_Дымченко_Иван", avatar: "24.jpg", photos: [] },
-  { id: "13", name: "Егоров Дима", folder: "13_Егоров_Дима", avatar: "24.jpg", photos: [] },
+  { id: "13", name: "Егоров Дима", folder: "13_Егоров_Дима", avatar: "24.jpg", photos: ["1.jpg","2.jpg","3.jpg","4.jpg","5.jpg","6.jpg","7.jpg","8.jpg","9.jpg","10.jpg",
+"11.jpg","12.jpg","13.jpg","14.jpg","15.jpg","16.jpg","17.jpg","18.jpg","19.jpg","20.jpg",
+"21.jpg","22.jpg","23.jpg","24.jpg","25.jpg","26.jpg","27.jpg","28.jpg","29.jpg","30.jpg",
+"31.jpg","32.jpg","33.jpg","34.jpg","35.jpg","36.jpg","37.jpg","38.jpg","39.jpg","40.jpg"] },
   { id: "14", name: "Зайцева Анастасия", folder: "14_Зайцева_Анастасия", avatar: "24.jpg", photos: [] },
   { id: "15", name: "Захарова Анастасия", folder: "15_Захарова_Анастасия", avatar: "24.jpg",  photos: [] },
   { id: "16", name: "Казаков Дмитрий", folder: "16_Казаков_Дмитрий", avatar: "24.jpg", photos: [] },
